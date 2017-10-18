170f483f7453b634b96eb8f723cd0368caba7113
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2017-10-18 23:05:09 +0100

    Added Map activity and changes to the main theme

--- app/src/main/java/me/pavelgeorgiev/songle/MainActivity.kt
+++ app/src/main/java/me/pavelgeorgiev/songle/MainActivity.kt
@@ -1,12 +1,20 @@
 package me.pavelgeorgiev.songle
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onMapSelect(view : View){
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
     }
 }
