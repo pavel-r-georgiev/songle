8a55bb6598ece8dbb305a82c40caccb02331f2be
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2017-10-25 01:12:05 +0100

    Added side navigation

--- app/src/main/java/me/pavelgeorgiev/songle/MapsActivity.kt
+++ app/src/main/java/me/pavelgeorgiev/songle/MapsActivity.kt
@@ -1,6 +1,7 @@
 package me.pavelgeorgiev.songle
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.support.v7.app.AppCompatActivity
@@ -19,14 +20,20 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.location.LocationListener;
 import android.os.Build
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.Gravity
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import com.google.android.gms.maps.model.*
 import com.google.maps.android.data.kml.KmlLayer
 import org.xmlpull.v1.XmlPullParserException
 import java.io.IOException
-import java.io.LineNumberReader
+import kotlinx.android.synthetic.main.activity_maps.*
 
 
 @Suppress("DEPRECATION")
@@ -46,13 +53,18 @@ class MapsActivity :
     private lateinit var mSongNumber: String
     private var mLyrics = HashMap<Int, List<String>>()
     private var mCurrLocationMarker: Marker? = null
+    private lateinit var mDrawerList: ListView
+    private lateinit var mAdapter: ArrayAdapter<String>
+    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var mDrawerToggle: ActionBarDrawerToggle
+
+    val MENU_ARRAY = arrayOf("Songs List", "Lyrics of current song")
     val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     val TAG = "MapsActivity"
     val LOCATION_REQUEST_INTERVAL: Long = 5000
     val LOCATION_REQUEST_FASTEST_INTERVAL: Long = 1000
     val KML_TYPE = "KML"
     val TXT_TYPE = "TXT"
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,14 +75,16 @@ class MapsActivity :
                 .findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        buildDrawerNav()
         buildGoogleApiClient()
 
         mSongNumber = intent.getStringExtra("NUMBER")
 
         var baseUrl = "${getString(R.string.maps_base_url)}/$mSongNumber"
         DownloadFileService(this, KML_TYPE).execute("$baseUrl/map5.kml")
-        DownloadFileService(this,  TXT_TYPE).execute("$baseUrl/words.txt")
+        DownloadFileService(this, TXT_TYPE).execute("$baseUrl/words.txt")
     }
+
 
     override fun onStart() {
         super.onStart()
@@ -88,7 +102,7 @@ class MapsActivity :
         super.onPause()
 
 //        Stop location updates when Activity is not active
-        if(mGoogleApiClient != null){
+        if (mGoogleApiClient != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
         }
 
@@ -118,8 +132,7 @@ class MapsActivity :
                 //Request Location Permission
                 checkLocationPermission()
             }
-        }
-        else {
+        } else {
             mMap.isMyLocationEnabled = true
         }
 
@@ -133,10 +146,10 @@ class MapsActivity :
      */
     @Throws(XmlPullParserException::class, IOException::class)
     override fun downloadComplete(bytes: ByteArray, fileType: String) {
-        if(fileType == KML_TYPE) {
-           onKmlDownload(bytes)
+        if (fileType == KML_TYPE) {
+            onKmlDownload(bytes)
         }
-        if(fileType == TXT_TYPE){
+        if (fileType == TXT_TYPE) {
             onTxtDownload(bytes)
         }
     }
@@ -144,16 +157,18 @@ class MapsActivity :
     private fun onTxtDownload(bytes: ByteArray) {
         val inputStream = bytes.inputStream()
 
-        inputStream.bufferedReader().useLines { lines -> lines.forEach {
-            parseLine(it)
-        } }
+        inputStream.bufferedReader().useLines { lines ->
+            lines.forEach {
+                parseLine(it)
+            }
+        }
     }
 
     private fun parseLine(line: String) {
         val lineList = line.trim().split("\\s+".toRegex())
         val lineNumber = lineList[0].toInt()
 
-        if(lineList.size < 2){
+        if (lineList.size < 2) {
             return
         }
 
@@ -161,20 +176,20 @@ class MapsActivity :
         mLyrics.put(lineNumber, lineWords)
     }
 
-    private fun onKmlDownload(bytes: ByteArray){
+    private fun onKmlDownload(bytes: ByteArray) {
         mLayer = KmlLayer(mMap, bytes.inputStream(), applicationContext)
         mLayer.addLayerToMap()
         val containers = mLayer.containers
 
         mLayer.setOnFeatureClickListener({ feature ->
-                val wordCoord = feature.getProperty("name").split(":").map { it.toInt() }
-                val line = wordCoord[0]
-                val word = wordCoord[1] - 1
+            val wordCoord = feature.getProperty("name").split(":").map { it.toInt() }
+            val line = wordCoord[0]
+            val word = wordCoord[1] - 1
 
             Log.i("KmlClick", "Feature clicked: " + feature.getProperty("name"))
             println(mLyrics[line].toString())
             println(mLyrics[line]?.get(word))
-
+//            addWord(mLyrics[line]?.get(word)
         })
     }
 
@@ -182,7 +197,7 @@ class MapsActivity :
      * Invoked once GoogleApiClient is connected.
      * If location permission is given request location update.
      */
-    override fun onConnected(connectionHint: Bundle?){
+    override fun onConnected(connectionHint: Bundle?) {
         val mLocationRequest = LocationRequest()
         mLocationRequest.interval = LOCATION_REQUEST_INTERVAL
         mLocationRequest.fastestInterval = LOCATION_REQUEST_FASTEST_INTERVAL
@@ -190,7 +205,7 @@ class MapsActivity :
 
         val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
 
-        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
         }
     }
@@ -265,13 +280,13 @@ class MapsActivity :
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
 
-        if(hasFocus){
+        if (hasFocus) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
     }
 
@@ -282,6 +297,38 @@ class MapsActivity :
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build()
+    }
+
+    private fun buildDrawerNav() {
+        mDrawerList = navList as ListView
+
+        mAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, MENU_ARRAY)
+        mDrawerList.adapter = mAdapter
+        mDrawerLayout = drawer_layout
+
+        mDrawerToggle = ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close)
+        mDrawerLayout.addDrawerListener(mDrawerToggle)
+        mapMenuButton.setOnClickListener({ toggleMenu(it) })
+
+        mDrawerList.setOnItemClickListener({ _: AdapterView<*>, _: View, position: Int, _: Long ->
+            when(position) {
+                0 -> startActivity(Intent(this, MainActivity::class.java))
+            }}
+        )
+    }
+
+    private fun toggleMenu(view: View) {
+        if (mDrawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            mDrawerLayout.closeDrawer(Gravity.LEFT);
+        } else {
+            mDrawerLayout.openDrawer(Gravity.LEFT);
+        }
+    }
+
+     fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+        when(position){
+            0 -> startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 
     override fun onConnectionSuspended(flag : Int) {
